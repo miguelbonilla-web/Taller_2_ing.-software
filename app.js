c57eab13aae7979351ee8c7fc9c7bd2f42bfbd62
Miguel <miguel.bonilla@estudiantesunibague.edu.co>
2026-08-20 21:14:21 -0500

    feat(optimizacion): agrega optimizacion para no repetir el saludo

--- app.js
+++ app.js
@@ -11,3 +11,8 @@ function resaltarMenuActivo() {
 }
 
 document.addEventListener("DOMContentLoaded", resaltarMenuActivo);
+
+let yaSaludo = false;
+function saludarUnaVez() {
+  if (!yaSaludo) { saludar(); yaSaludo = true; }
+}
